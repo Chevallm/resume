0e411057e14bc4c8322389adedc0202575515fc2
Maxime CHEVALLIER-PICHON <maxime.chevallier_pichon@yahoo.fr>
2022-02-05 11:52:38 +0100

    start the home card

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,20 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 
+gulp.task('serve', () => {
+    browserSync.init({
+        server: './'
+    });
+    gulp.watch(['src/**/*.scss'], gulp.series('sass'));
+    gulp.watch(['src/**/*.html']).on('change', gulp.series('html', browserSync.reload));
+    
+});
+
 gulp.task('sass', () => {
     return gulp.src('src/scss/style.scss')
     .pipe(sass())
     .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('html', () => {
@@ -13,19 +23,16 @@ gulp.task('html', () => {
     .pipe(gulp.dest('./'))
 });
 
+gulp.task('images', () => {
+    return gulp.src('src/images/*')
+    .pipe(gulp.dest('dist/images'));
+});
+
 gulp.task('fonts', () => {
     return gulp.src('src/fonts/*')
     .pipe(gulp.dest('dist/fonts'));
 });
 
-gulp.task('watch', () => {
-    gulp.watch(['src/**/*.html'], gulp.series('html', browserSync.reload));
-    gulp.watch(['src/**/*.scss'], gulp.series('sass', browserSync.reload));
-    browserSync.init({
-        server: './'
-    });
-});
 
 
-
-gulp.task('default', () => gulp.parallel('html', 'sass', 'fonts'));
+gulp.task('default', () => gulp.parallel('html', 'sass', 'fonts', 'images'));
